20c060e0977f3360fb8243371046f97cda6a645f
Abhishek Pareek <apareek@wetafx.co.nz>
2020-12-04 18:09:57 +1300

    Working implementation of Twitter FanOut Consumer to an AWS Data Stream Shard

---
+++ src/main/java/com/makeittotop/aws/kinesis/TwitterFanOutConsumerArgs.java
@@ -0,0 +1,53 @@
+package com.makeittotop.aws.kinesis;
+
+import com.beust.jcommander.Parameter;
+
+public class TwitterFanOutConsumerArgs {
+    @Parameter(
+            names = {"--shardId", "-sh"},
+            description = "Shard ID",
+            arity = 1,
+            required = true
+    )
+    private String shardId;
+
+    public String getShardId() {
+        return shardId;
+    }
+
+    public void setShardId(String shardId) {
+        this.shardId = shardId;
+    }
+
+    public String getConsumerName() {
+        return consumerName;
+    }
+
+    public void setConsumerName(String consumerName) {
+        this.consumerName = consumerName;
+    }
+
+    public String getStreamArn() {
+        return streamArn;
+    }
+
+    public void setStreamArn(String streamArn) {
+        this.streamArn = streamArn;
+    }
+
+    @Parameter(
+            names = {"--consumerName", "-c"},
+            description = "Consumer Name",
+            arity = 1,
+            required = false
+    )
+    private String consumerName;
+
+    @Parameter(
+            names = {"--streamArn", "-sa"},
+            description = "AWS Data Stream ARN",
+            arity = 1,
+            required = true
+    )
+    private String streamArn;
+}
